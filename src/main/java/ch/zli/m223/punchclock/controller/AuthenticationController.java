c46ab42f6b23d16cf71a01aa627840c1698eee33
ZLI <robertallandavidanderson@gmail.com>
2021-11-22 16:43:09 +0100

    Added documentation for the Authentication interface

--- src/main/java/ch/zli/m223/punchclock/controller/AuthenticationController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/AuthenticationController.java
@@ -6,15 +6,21 @@ import javax.ws.rs.core.MediaType;
 
 import ch.zli.m223.punchclock.domain.User;
 import ch.zli.m223.punchclock.service.AuthenticationService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/auth")
+@Tag(name = "Authentication", description = "Handling of user authentication")
 public class AuthenticationController{
     @Inject
     AuthenticationService authenticationService;
+
+
     @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
+    @Operation(summary = "Checks if the given user data exists, then returns a JWT Token.", description = "Compares the given user data against existing users in the database. If a match is found a JWT Token is generated and returned. If no matching user is a found a NotAuthorizedException is thrown.")
     public String login(User user){
         if(authenticationService.checkIfUserExists(user)){
             return authenticationService.generateValidJwtToken(user.getUsername());
